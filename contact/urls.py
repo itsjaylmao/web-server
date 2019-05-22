a1072e411b3befca1676d5dc6965b4b842987f1c
Nate Moore <humanevolution42@gmail.com>
2019-05-23 00:10:35 +0800

    Changes to Contact forms and views

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import contactView, successView
+from .views import contactView
 
 urlpatterns = [
     path('', contactView, name="contact"),
-    path('success/', successView, name="success"),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.conf import settings
 
 from .forms import ContactForm
@@ -9,19 +9,18 @@ def contactView(request):
     if request.method == 'GET':
         form = ContactForm()
     else:
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            subject = "New Enquiry: " + form.cleaned_data['subject']
-            message = form.cleaned_data['message']
-            from_email = form.cleaned_data['from_email']
-            from_name = "Name: " + form.cleaned_data['from_name']
-            body = from_name + " " + "Email: " + from_email + " " + message
-            try:
-                send_mail(subject, body, from_email, ['nate@natemoore.me'])
-            except BadHeaderError:
-                return HttpResponse('Bad header')
-            return redirect('success')
+        if request.method == "POST" and request.is_ajax():
+            form = ContactForm(request.POST)
+            if form.is_valid():
+                subject = "New Enquiry: " + form.cleaned_data['subject']
+                message = form.cleaned_data['message']
+                from_email = form.cleaned_data['from_email']
+                from_name = "Name: " + form.cleaned_data['from_name']
+                body = from_name + " " + "Email: " + from_email + " " + message
+                try:
+                    send_mail(subject, body, from_email, ['nate@natemoore.me'])
+                except BadHeaderError:
+                    return HttpResponse('Bad header')
+                return JsonResponse({"success": True}, status=200)
+        return JsonResponse({"success": False}, status=400)
     return render(request, 'contact/contact.html', {'form': form})
-
-def successView(request):
-    return HttpResponse('Success! Thanks for getting in touch.') 
